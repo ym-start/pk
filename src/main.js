938cf15e235fa2385735bdb16e2669777ea91512
xy <361474778@qq.com>
2017-05-05 08:45:33 +0800

    第一次修改

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+require('./styles/app.scss')
+
+import Vue from 'vue'
+
+import index from './scripts/components/index.vue'
+
 new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+  components: { index },
+  template: '<index />'
 })
---
+++ src/scripts/components/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="m-index">
+    <header>
+      header
+    </header>
+    <section>
+
+    </section>
+    <nav>
+
+    </nav>
+  </div>
+</template>
